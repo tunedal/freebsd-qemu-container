e54934d49611903c2c8dbc3beef7984b9f66b127
Henrik Tunedal <tunedal@gmail.com>
2024-05-17 13:03:35 +0200

    Add Python 3.12 to Python image
    FreeBSD doesn't package version 3.12, so this is built from source.

--- python/customize.sh
+++ python/customize.sh
@@ -12,4 +12,23 @@ ASSUME_ALWAYS_YES=yes pkg -r /mnt install \
                       python310 py310-sqlite3 \
                       python311 py311-sqlite3
 
+# Install dependencies for building Python.
+ASSUME_ALWAYS_YES=yes pkg install \
+                      pkgconf sqlite3 libffi readline
+
+# Build Python 3.12.
+PATH="$PATH:/usr/local/sbin:/usr/local/bin"
+hash=56bfef1fdfc1221ce6720e43a661e3eb41785dd914ce99698d8c7896af4bdaa1
+pydist=Python-3.12.3
+exebasename=python3.12
+url=https://www.python.org/ftp/python/3.12.3/$pydist.tar.xz
+fetch "$url"
+sha256 -c "$hash" "$pydist.tar.xz" || exit 1
+tar Jxvf "$pydist.tar.xz"
+cd "$pydist"
+./configure --prefix="/opt/$pydist" --enable-optimizations
+make
+DESTDIR="/mnt/$pydist" make install
+ln -s "/opt/$pydist/bin"/{$exebasename,$exebasename-config} /mnt/usr/local/bin/
+
 poweroff
